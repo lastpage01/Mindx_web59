bab66329d4840a9e8d4e1564a3bf92af421cd070
Quy <quygiang2001@gmail.com>
2022-06-04 16:13:31 +0700

    f

--- SESSION_1/NODEJS_SESSION_1/index.js
+++ SESSION_1/NODEJS_SESSION_1/index.js
@@ -1,21 +1,23 @@
-function HelloWord(){
-    function timeout(){
-        setTimeout(() => {
-            console.log("time out");
-        }, 10);
-    }
+// const fs = require("fs");
+const { sum,sub } = require("./custom_module/math");
+// fs.readFile("./Mindx_1.txt", "utf-8", (err, data) => {
+//   if (err) {
+//     console.log("error of fs function: ", err);
+//   }
+//   console.log("data: ", data);
+// });
 
-    function timeout222(){
-        setTimeout(() => {
-            console.log("time out 222");
-        }, 11);
-    }
-    console.log("1");
-    timeout222();
-    timeout();
-    console.log("2");
-    console.log("3");
-    console.log("4");
-}
+// // CRUD : create , delete, update, read
+// const data = fs.readFileSync("./students.json");
+// // console.log("data: ", JSON.parse(data));
 
-HelloWord();
+// const students = JSON.parse(data);
+// const newStudent = {
+//   id: 2,
+//   name: "Giap",
+//   gender: "F",
+// };
+// students["data"].push(newStudent);
+// fs.writeFileSync('./students.json',JSON.stringify(students));
+
+console.log(sub(1, 2));
